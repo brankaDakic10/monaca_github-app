18a8efdc1d097fe7d109c64054f626f1bb247b1f
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-29 21:39:11 +0200

    Added 3.Task

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,25 @@
     <v-ons-page>
       
        <app-toolbar />
-      
-     
+       <div class="content">
+      <app-search :query.sync="query" placeholder="Type username "
+      />
+     </div>
     </v-ons-page>
 </template>
 <script>
 import AppToolbar from './components/AppToolbar.vue'
+import AppSearch from './components/AppSearch.vue'
   export default{
    components:{
-     AppToolbar
+     AppToolbar,
+     AppSearch
    },
-    
+    data(){
+      return{
+        query:""
+      }
+    },
     methods: {
       alert() {
         this.$ons.notification.alert('This is an Onsen UI alert notification test.');
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <v-ons-search-input
+      :placeholder="placeholder"
+      style="width: 100%;"
+      :value="query"
+      @input="$emit('update:query', $event.target.value)">
+    </v-ons-search-input>
+
+
+</template>
+
+<script>
+export default {
+  name:"AppSearch",
+  props:{
+      query:String,
+       placeholder:String      
+  }
+}
+</script>
+
+<style>
+
+</style>
