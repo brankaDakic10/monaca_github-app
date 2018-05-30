20e00d7012aba1ab9cc8fd6c7f800dae6185ca17
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-30 09:13:18 +0200

    Added changes in 4.Task

--- src/services/GitHub.js
+++ src/services/GitHub.js
@@ -7,7 +7,7 @@ export default class GitHub {
     axios.defaults.baseURL = `https://api.github.com/`
   }
   getRepos (username){
-      return axios.get('users/${username}/repos',username)
+      return axios.get(`users/${username}/repos`, username)
   }
  
 
