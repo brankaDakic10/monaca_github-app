8f5c68df5dfb387faa36c247dfe7143f3b381be0
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-31 20:14:03 +0200

    Task 5. and Task 6.

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,87 @@
        <app-toolbar />
        <div class="content">
       <app-search :query.sync="query" placeholder="Type username "
-      />
+      /> 
+ 
+
+<v-ons-list-header>Repositories of {{query}}</v-ons-list-header>
+      <v-ons-list-item v-for="repo in repos" :key="repo.id" v-if="repo">
+        <div class="left">
+          <img class="list-item__thumbnail" :src="image">
+        </div>
+        <div class="center">
+          <span class="list-item__title">{{repo.name}}</span>
+          <span class="list-item__subtitle">{{repo.description}}</span>
+           <span class="list-item__subtitle">{{repo.updated_at}}</span>
+
+        </div>
+      </v-ons-list-item>
      </div>
+       
+  
+      
     </v-ons-page>
 </template>
 <script>
-import AppToolbar from './components/AppToolbar.vue'
-import AppSearch from './components/AppSearch.vue'
+import debounce from "lodash/debounce"
+import { gitHub } from "./services/GitHub.js"
+import AppToolbar from "./components/AppToolbar.vue"
+import AppSearch from "./components/AppSearch.vue"
+
+
   export default{
    components:{
      AppToolbar,
      AppSearch
+    
    },
     data(){
       return{
-        query:""
+        query:"",
+        image:'',
+       isSearchDone:false,
+       
+        repos:[]
       }
     },
-    methods: {
-      alert() {
-        this.$ons.notification.alert('This is an Onsen UI alert notification test.');
-      }
+methods: {
+    getRepos: debounce(function () {
+         
+       gitHub.getRepos(this.query)
+        .then(response => {
+           
+          this.repos = response.data;
+          console.log('see repos info',this.repos);
+          this.isSearchDone = true;
+         this.repos.forEach(repo => {
+         this.image = repo.owner.avatar_url;
+         });
+   
+          
+        })
+        .catch(errors => {
+          console.log(errors);
+        });
+    }, 500)
+  },
+  watch: {
+    query: function() {
+   
+      this.getRepos()
+      this.isSearchDone = false;
     }
-  };
+  }
+  }
 </script>
+  
+    // getRepos() {
+    //   gitHub.getRepos(this.query).then(response => {
+    //     console.log(response)
+    //   })
+    // }
+
+    
+  
+
+  
+
--- src/services/GitHub.js
+++ src/services/GitHub.js
@@ -1,18 +1,24 @@
 
 import axios from 'axios'
 
+
+const API_TOKEN='96352575dc9820e6b46298531017871732b49e7e'
+
 export default class GitHub {
   constructor () {
                                              
-    axios.defaults.baseURL = `https://api.github.com/`
+    axios.defaults.baseURL = 'https://api.github.com/'
+    // add token
+    axios.defaults.headers.common['Authorization'] = `Bearer  ${API_TOKEN}`
+  
   }
   getRepos (username){
-      return axios.get(`users/${username}/repos`, username)
+      return axios.get(`users/${username}/repos`)
   }
  
 
 }
-export const github = new GitHub()
+export const gitHub = new GitHub()
 
 
  
